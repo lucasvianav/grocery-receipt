e8c2c8ed6e183bb69b8615b30339016c34db4d7e
Lucas Viana <oviana@outlook.com>
2022-07-12 02:40:51 -0300

    Clean up main.py + config tesseract

--- src/main.py
+++ src/main.py
@@ -1,49 +1,33 @@
 import cv2
 import pytesseract
+from numpy.typing import NDArray
 
 import utils
 
 if __name__ == "__main__":
-    original_img = cv2.imread("receipts/no-shadow.jpg", cv2.IMREAD_GRAYSCALE)
+    original_img: NDArray = cv2.imread("receipts/cropped.jpg", cv2.IMREAD_GRAYSCALE)
 
-    # arbitrary_img = utils.binarize(original_img, 105)
-    # scratch_otsu_img = utils.otsu(original_img)
-    _, cv2_otsu_img = cv2.threshold(
-        original_img,
+    # apply otsu binarization to the pre-processed image
+    otsu_img: NDArray
+    _, otsu_img = cv2.threshold(
+        utils.clean(original_img),
         0,
         255,
         cv2.THRESH_BINARY + cv2.THRESH_OTSU,
     )
-    # adaptive_mean_img = cv2.adaptiveThreshold(
-    #     original_img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 25, 2
-    # )
-    adaptive_gaussian_img = cv2.adaptiveThreshold(
-        original_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 25, 2
-    )
-    blurred_adaptive_gaussian_img = cv2.medianBlur(adaptive_gaussian_img, 7)
+
+    # apply morphological opening in order to close gaps
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (2, 2))
+    otsu_img = cv2.morphologyEx(otsu_img, cv2.MORPH_OPEN, kernel)
 
     utils.plot(
         [
             {"title": "Original image", "image": original_img},
-            # {
-            #     "title": "Binary Image (arbitrary threshold = 105)",
-            #     "image": arbitrary_img,
-            # },
-            # {"title": "Scratch Otsu", "image": scratch_otsu_img},
-            {"title": "OpenCV Otsu", "image": cv2_otsu_img},
-            # {"title": "OpenCV Adaptive Mean Thresholding", "image": adaptive_mean_img},
-            {
-                "title": "OpenCV Adaptive Gaussian Thresholding",
-                "image": adaptive_gaussian_img,
-            },
-            {
-                "title": "OpenCV Adaptive Gaussian Thresholding (blurred)",
-                "image": blurred_adaptive_gaussian_img,
-            },
+            {"title": "Otsu", "image": otsu_img},
         ],
         1,
-        4,
+        2,
         False,
     )
 
-    print(pytesseract.image_to_string(cv2_otsu_img, lang="por"))
+    print(pytesseract.image_to_string(otsu_img, lang="por", config="--oem 2 --psm 3"))
